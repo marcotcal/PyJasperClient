ad1bd50d8bf997e5930f646fa99b3371730afc09
marcotc <marcotc.al@gmail.com>
2021-02-04 20:38:46 +0000

    Login and Logout

--- jasperclient/jasperclient.py
+++ jasperclient/jasperclient.py
@@ -1,13 +1,57 @@
+import requests
+from xml.etree import ElementTree
+import base64
+import json
 
 
 class JasperClient:
 
     def __init__(self, server_url):
         self.url = server_url
+        self.info = None
+        self.session = requests.session()
+        self.connected = False
 
     def __del__(self):
         pass
 
-    @staticmethod
-    def server_info():
-        return 'tests'
+    def server_info(self):
+        cmd_url = self.url + 'rest_v2/serverInfo'
+        request = requests.get(cmd_url)
+        self.info = request.content
+
+    @property
+    def server_version(self):
+        cmd_url = self.url + 'rest_v2/serverInfo/version'
+        req = requests.get(cmd_url, cookies=self.login_cookies)
+        if req.ok:
+            return req.content.decode(req.encoding)
+        else:
+            return None
+
+    def logout(self):
+        cmd_url = self.url + 'logout.html'
+        self.session.get(cmd_url)
+        self.session.cookies.clear()
+        self.connected = False
+
+    def login(self, user, password):
+        params = {"j_username": user, "j_password": password}
+        cmd_url = self.url + 'rest_v2/login'
+
+        r = self.session.post(url=cmd_url, data=params)
+        if r.status_code == 200:
+            self.connected = True
+        else:
+            self.connected = False
+        return self.connected
+
+    def resources(self):
+        cmd_url = self.url + 'rest_v2/resources/'
+        headers = {'content-type': 'application/json'}
+        request = requests.get(cmd_url, auth=(self.auth_user, self.auth_password), headers=headers)
+
+        if request.ok:
+            return request.content
+        else:
+            return None
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,14 +1,20 @@
 import unittest
 from jasperclient import jasperclient
 
+__TEST_URL__ = 'http://your_domain:8080/jasperserver/'
+__SERVER_VERSION__ = '7.2.0'
+
 
 class JasperTest(unittest.TestCase):
 
     def setUp(self):
-        self.client = jasperclient.JasperClient('http://your_domain.com')
+        self.client = jasperclient.JasperClient(__TEST_URL__)
 
-    def test_server_information(self):
-        self.assertIsNotNone(self.client.server_info(), "Error getting server information")
+    def test_server_version(self):
+        self.assertIsNotNone(self.client.server_version, "Error getting server information")
+
+    def test_server_version(self):
+        self.assertEqual(self.client.server_version, __SERVER_VERSION__, "Not the expected version")
 
 
 if __name__ == '__main__':
